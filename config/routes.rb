0da743c77828921c0d49b4c278dbe88702f728af
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2023-01-13 01:14:36 -0500

    Checkout button

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "products#index"
   resources :products
+  resources :checkout, only: [:create]
+  post "checkout/create", to: "checkout#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
